05d760edfc0bd642ae22f90745d10d171155c742
José Nobre <41454761+JosePedroNobre@users.noreply.github.com>
2021-02-07 15:38:48 +0000

    final changes

--- app/src/main/java/com/emel/app/network/api/requests/TokenRequest.kt
+++ app/src/main/java/com/emel/app/network/api/requests/TokenRequest.kt
@@ -1,5 +1,9 @@
 package com.emel.app.network.api.requests
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class TokenRequest(
     val token: String
-)
+):Parcelable
